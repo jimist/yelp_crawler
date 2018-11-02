95da1927d2a3e37a3499f8013c999a9f15a75bf8
jimist <arash.jeem@gmail.com>
2018-11-02 23:26:04 +0330

    connected crawlData to database

--- Crawlers/Crawler.py
+++ Crawlers/Crawler.py
@@ -1,10 +1,28 @@
+from DataProviders import DataProviderService
+from Models import CrawlData
+import requests
+
 class Crawler():
 	def __init__(self):
-		print("init base crawler")
+		self.session = DataProviderService.getSession()
 
-	def checkDatabaseForUrl(url):
-		print("check database for url")
-		
+	def checkDatabaseForUrl(self, url):
+		result = self.session.query(CrawlData).filter(CrawlData.url == url).first()
+		if result is None:
+			return False
+		return result
 
-	def getData(url):
-		print("check database for url, send request and save in database if does not exist")
+	def getData(self, url):
+		data = self.checkDatabaseForUrl(url)
+		if data is False:
+			tempCrawlData = CrawlData()
+			tempCrawlData.url = url
+
+			response = requests.get(url)
+			tempCrawlData.body = response.text
+
+			self.session.add(tempCrawlData)
+			self.session.commit()
+			return tempCrawlData
+		else:
+			return data.body
